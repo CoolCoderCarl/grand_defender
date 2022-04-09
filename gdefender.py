8693bc1196f8facf1b5c49a2249290c6d0594731
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-09 19:23:48 +0300

    Update logic; check system before shutdown

--- gdefender.py
+++ gdefender.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import platform
 import threading
 
 warning = """
@@ -11,42 +12,82 @@ warning = """
             3) If you close this window
 """
 
-print(warning)
-
 timer_msg = "Time is up !"
-shutdown_cmd = "shutdown /s /t 1"
+shutdown_win = "shutdown /s /t 1"
+shutdown_nix = "shutdown now -h"
 
 
 def is_timer_finished(t=10):
+    """
+    Return True when timer is finished
+    :param t:
+    :return:
+    """
     while t > 0:
         t -= 1
         time.sleep(1)
     return True
 
 
-def timer():
-    if is_timer_finished():
-        print(timer_msg)
-        sys.exit()
-        # os.system(shutdown_cmd)
-
-
 def get_pass():
+    """
+    Read password from file
+    :return:
+    """
     password_file = open("password", "r")
     password = password_file.read()
     return password
 
 
-def shutdown_check():
+def is_linux():
+    """
+    Check is current system Linux
+    :return:
+    """
+    if 'Linux' in platform.system():
+        return True
+    else:
+        return False
+
+
+def shutdown_system():
+    """
+    Shutdown the system
+    Win or nix
+    :return:
+    """
+    if is_linux():
+        os.system(shutdown_nix)
+    else:
+        print("FAIL")
+        os.system(shutdown_win)
+
+
+def timer():
+    """
+    If time is up, notify user about it and shutdown system
+    :return:
+    """
+    if is_timer_finished():
+        print(timer_msg)
+        shutdown_system()
+
+
+def is_pass_correct():
+    """
+    Check is password is correct
+    If not shutdown system
+    :return:
+    """
     input_password = input("Password:")
     if input_password == get_pass():
         sys.exit()
     else:
-        print("FAIL")
-        sys.exit()
-        # os.system(shutdown_cmd)
+        shutdown_system()
 
 
 if __name__ == '__main__':
 
-    shutdown_check()
+    print(warning)
+    # Run timer
+    # Check pass
